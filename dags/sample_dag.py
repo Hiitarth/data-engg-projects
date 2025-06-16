4f1430753037d31de227f518d49b67a38c48f73d
Hiitarth <90138345+Hiitarth@users.noreply.github.com>
2025-06-16 21:23:52 +0530

    Update sample_dag.py

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # Define the Python function
 def say_hello():
-    print("Hel lo, HAstro n ome  r!!")
+    print("Hel lo, HAstron ome  r!!")
 
 # Define the DAG
 with DAG(
