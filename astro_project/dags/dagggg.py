7a5770b615a7d51dfa6736e63d5e890dce402f29
Hiitarth <90138345+Hiitarth@users.noreply.github.com>
2025-06-19 01:54:49 +0530

    Create dagggg.py

---
+++ astro_project/dags/dagggg.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+def say_hello():
+    print("Hello, Astronomer this!")
+
+
+default_args = {
+    'start_date': datetime(2024, 1, 1),
+}
+
+
+with DAG(
+    dag_id='hello_astro_dag',
+    schedule='@daily', 
+    default_args=default_args,
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    hello_task = PythonOperator(
+        task_id='say_hello_task',
+        python_callable=say_hello,
+    )
+
+hello_task
